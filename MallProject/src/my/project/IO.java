0e70600b9b588a23d890cad1a4d3d178e33fb18a
shawn-lacarta <lacarta@noseryoung.com>
2021-10-21 17:02:35 +0200

    outsourced project

--- MallProject/src/my/project/IO.java
+++ MallProject/src/my/project/IO.java
@@ -1,10 +1,32 @@
 package my.project;
 
+import java.util.Scanner;
+
 /**
  * This class has all the fix output of the program.
  * @author shawn
  */
 public class IO {
+
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    private Scanner scan = new Scanner(System.in);
+    private final int CLEANING_SERVICE = 500;
+    private final int GARBAGE_COLLECTOR = 850;
+    private final int OFFICE_WORKER = 1200;
+    private final int INVESTOR = 2000;
+    private final int CEO = 5000;
+    private double budget;
+    private ShoppingCenter s;
+    private Manager m;
+    private Store currentLocation;
+
+    public IO(ShoppingCenter s, Manager m) {
+        this.s = s;
+        this.m = m;
+    }
 
     /**
      * This method is for my border box. I implemented this method so i can give my program a
@@ -60,11 +82,154 @@ public class IO {
     }
 
     /**
+     * The user can choose his profession in this method by entering a number, which is stored a
+     * profession. Depending on the profession, the budget is set to a different value.
+     *
+     */
+    public void professionChoice() {
+        int input = 0;
+        while (input < 1 || input > 5) {
+            IO.outPutListInBox(new String[]{"choose your profession: ", " ", "[1] cleaning service", "[2] garbage collector", "[3] office worker", "[4] investor", "[5] CEO"}, 2);
+            input = m.readInt();
+            switch (input) {
+                case 1 -> budget = CLEANING_SERVICE;
+                case 2 -> budget = GARBAGE_COLLECTOR;
+                case 3 -> budget = OFFICE_WORKER;
+                case 4 -> budget = INVESTOR;
+                case 5 -> budget = CEO;
+                default -> System.out.println("wrong input");
+            }
+        }
+        System.out.println(ANSI_RED + "your budget: " + budget + " CHF" + ANSI_RESET);
+    }
+
+    /**
+     * Here the user can decide whether to enter the mall or prefer to work. The
+     * user decides by spelling out enter or work.
+     *
+     */
+    public void enterMall() {
+        String input = " ";
+        currentLocation = m.findHallway(s.getFloors().get(0));
+
+        while (!(input.equals("enter"))) {
+
+            IO.outPutListInBox(new String[]{"to enter the mall spell enter / to work spell work / to leave spell exit: "}, 2);
+            input = scan.nextLine();
+
+            switch (input) {
+                case "enter" -> System.out.println(ANSI_BLUE + " _    _      _                            _          _   _            ______           _      _      ___  ___      _ _ \n" +
+                        "| |  | |    | |                          | |        | | | |          |___  /          (_)    | |     |  \\/  |     | | |\n" +
+                        "| |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___     / / _   _ _ __ _  ___| |__   | .  . | __ _| | |\n" +
+                        "| |/\\| |/ _ | |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\  | __| '_ \\ / _ \\   / / | | | | '__| |/ __| '_ \\  | |\\/| |/ _` | | |\n" +
+                        "\\  /\\  |  __| | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/ ./ /__| |_| | |  | | (__| | | | | |  | | (_| | | |\n" +
+                        " \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/   \\__|_| |_|\\___| \\_____/\\__,_|_|  |_|\\___|_| |_| \\_|  |_/\\__,_|_|_|\n" +
+                        "                                                                                                                       \n" +
+                        "                                                                                                                       " + ANSI_RESET);
+                case "work" -> m.goWork();
+                case "exit" -> System.exit(0);
+                default -> System.out.println("wrong input");
+            }
+
+        }
+        move();
+
+    }
+
+    /**
+     * This method is to move around the mall. Depending on the user input, different methods are used.
+     *
+     */
+    public void move() {
+
+        int selectMovement = 1;
+        while (selectMovement >= 1 && selectMovement <= 5) {
+            IO.outPutListInBox(new String[]{"[1] shopping", "[2] our stores", "[3] map", "[4] shop other floor", "[5] exit",}, 2);
+            selectMovement = m.readInt();
+            switch (selectMovement) {
+                case 1 -> enterStore();
+                case 2 -> {
+                    System.out.println(ANSI_YELLOW + "our stores: " + ANSI_RESET);
+                    storesFirstFloor();
+                    storesSecondFloor();
+                }
+                case 3 -> {
+                    if (s.getFloors().indexOf(m.findFloor(currentLocation)) == 0) {
+                        printFirstFloor();
+                    } else {
+                        printSecondFloor();
+                    }
+                }
+                case 4 -> m.changeFloor();
+                case 5 -> enterMall();
+                default -> System.out.println("wrong input");
+            }
+        }
+    }
+
+    /**
+     * When the user decides to enter a store, this method is used. The user
+     * can decide whether to buy something or leave the store
+     *
+     */
+    public void enterStore() {
+        Floor currentFloor;
+        char storeMovement;
+        currentLocation = m.findHallway(currentFloor = m.findFloor(currentLocation));
+        m.findStore(currentFloor);
+        printCurrentPosition();
+        do {
+            IO.outPutListInBox(new String[]{"[b] buy item", "[l] leave store"}, 2);
+            storeMovement = scan.next().charAt(0);
+            switch (storeMovement) {
+                case 'b' -> items();
+
+                case 'l' -> currentLocation = m.findHallway(currentFloor);
+                default -> System.out.println("wrong input");
+            }
+        } while (storeMovement != 'l');
+
+    }
+
+    /**
+     * This method is to choose, which item the user wants to buy.
+     *
+     */
+    public void items() {
+        currentLocation.getProducts().forEach(product -> {
+            System.out.println();
+            IO.outPutListInBox(new String[]{product.getName(), product.getSize(), product.getColor(), "" + product.getPrice()}, 2);
+        });
+        System.out.println("what do you want to buy: ");
+        scan.nextLine();
+        String input = scan.nextLine();
+
+        currentLocation.getProducts().forEach(product -> {
+            if (input.equals(product.getName())) {
+                budget -= product.getPrice();
+            }
+        });
+
+        budget = Math.round(budget / 0.05) * 0.05;
+        System.out.printf("new budget: %.2f\n", budget);
+
+    }
+
+    /**
+     * This method is used to print the current location.
+     *
+     */
+    public void printCurrentPosition() {
+        System.out.println(ANSI_BLUE + "current location: " + ANSI_RESET + (1 + s.getFloors().indexOf(m.findFloor(currentLocation))) + " floor" + " " + currentLocation.getName());
+    }
+
+
+    /**
      * This method is to print the map for the first floor.
      *
      */
     public void printFirstFloor() {
-        System.out.println(Manager.ANSI_YELLOW + "1st floor map" + Manager.ANSI_RESET);
+        System.out.println(ANSI_YELLOW + "1st floor map" + ANSI_RESET);
         System.out.println("╔═════════════╦══════════╦════════════════════╗\n" +
                 "║             ║          ║                    ║\n" +
                 "║             ║          ║                    ║\n" +
@@ -101,7 +266,7 @@ public class IO {
      *
      */
     public void printSecondFloor() {
-        System.out.println(Manager.ANSI_YELLOW + "2nd floor map" + Manager.ANSI_RESET);
+        System.out.println(ANSI_YELLOW + "2nd floor map" + ANSI_RESET);
         System.out.println("╔═════════════╦══════════╦════════════════════╗\n" +
                 "║             ║          ║                    ║\n" +
                 "║             ║          ║                    ║\n" +
--- MallProject/src/my/project/Manager.java
+++ MallProject/src/my/project/Manager.java
@@ -6,162 +6,31 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * This class will manage the whole program.
+ *
  * @author shawn
  */
 public class Manager {
-
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_RESET = "\u001B[0m";
-    private Scanner scan = new Scanner(System.in);
-    private final int CLEANING_SERVICE = 500;
-    private final int GARBAGE_COLLECTOR = 850;
-    private final int OFFICE_WORKER = 1200;
-    private final int INVESTOR = 2000;
-    private final int CEO = 5000;
     private double budget;
     private ShoppingCenter s = new ShoppingCenter();
     private Store currentLocation;
+    private IO io;
 
 
     /**
      * This is my start method. First the user can choose his profession. Afterwards the user can enter the mall.
-     *
      */
     public void startProgram() {
-        professionChoice();
-        enterMall();
-    }
-
-    /**
-     * The user can choose his profession in this method by entering a number, which is stored a
-     * profession. Depending on the profession, the budget is set to a different value.
-     *
-     */
-    public void professionChoice() {
-        int input = 0;
-        while (input < 1 || input > 5) {
-            IO.outPutListInBox(new String[]{"choose your profession: ", " ", "[1] cleaning service", "[2] garbage collector", "[3] office worker", "[4] investor", "[5] CEO"}, 2);
-            try {
-                input = scan.nextInt();
-            } catch (Exception e) {
-
-            } finally {
-                scan.nextLine();
-            }
-
-            switch (input) {
-                case 1 -> budget = CLEANING_SERVICE;
-                case 2 -> budget = GARBAGE_COLLECTOR;
-                case 3 -> budget = OFFICE_WORKER;
-                case 4 -> budget = INVESTOR;
-                case 5 -> budget = CEO;
-                default -> System.out.println("wrong input");
-            }
-        }
-        System.out.println(ANSI_RED + "your budget: " + budget + " CHF" + ANSI_RESET);
-    }
-
-    /**
-     * Here the user can decide whether to enter the mall or prefer to work. The
-     * user decides by spelling out enter or work.
-     *
-     */
-    public void enterMall() {
-
-        String input = " ";
+        io = new IO(s, this);
         s.createShoppingCenter();
-        currentLocation = findHallway(s.getFloors().get(0));
-
-        while (!(input.equals("enter"))) {
-
-            IO.outPutListInBox(new String[]{"to enter the mall spell enter / to work spell work / to leave spell exit: "}, 2);
-            input = scan.nextLine();
-
-            switch (input) {
-                case "enter" -> System.out.println(ANSI_BLUE + " _    _      _                            _          _   _            ______           _      _      ___  ___      _ _ \n" +
-                        "| |  | |    | |                          | |        | | | |          |___  /          (_)    | |     |  \\/  |     | | |\n" +
-                        "| |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___     / / _   _ _ __ _  ___| |__   | .  . | __ _| | |\n" +
-                        "| |/\\| |/ _ | |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\  | __| '_ \\ / _ \\   / / | | | | '__| |/ __| '_ \\  | |\\/| |/ _` | | |\n" +
-                        "\\  /\\  |  __| | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/ ./ /__| |_| | |  | | (__| | | | | |  | | (_| | | |\n" +
-                        " \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/   \\__|_| |_|\\___| \\_____/\\__,_|_|  |_|\\___|_| |_| \\_|  |_/\\__,_|_|_|\n" +
-                        "                                                                                                                       \n" +
-                        "                                                                                                                       " + ANSI_RESET);
-                case "work" -> goWork();
-                case "exit" -> System.exit(0);
-                default -> System.out.println("wrong input");
-            }
-
-        }
-        move();
-
+        io.professionChoice();
+        io.enterMall();
     }
 
-    /**
-     * This method is to move around the mall. Depending on the user input, different methods are used.
-     *
-     */
-    public void move() {
-        IO o = new IO();
-        int selectMovement = 1;
-        while (selectMovement >= 1 && selectMovement <= 5) {
-            IO.outPutListInBox(new String[]{"[1] shopping", "[2] our stores", "[3] map", "[4] shop other floor", "[5] exit",}, 2);
-            try {
-                selectMovement = scan.nextInt();
-            } catch (Exception e) {
-
-            } finally {
-                scan.nextLine();
-            }
-            switch (selectMovement) {
-                case 1 -> enterStore();
-                case 2 -> {
-                    System.out.println(ANSI_YELLOW + "our stores: " + ANSI_RESET);
-                    o.storesFirstFloor();
-                    o.storesSecondFloor();
-                }
-                case 3 -> {
-                    if (s.getFloors().indexOf(findFloor(currentLocation)) == 0) {
-                        o.printFirstFloor();
-                    } else {
-                        o.printSecondFloor();
-                    }
-                }
-                case 4 -> changeFloor();
-                case 5 -> enterMall();
-                default -> System.out.println("wrong input");
-            }
-        }
-    }
-
-    /**
-     * When the user decides to enter a store, this method is used. The user
-     * can decide whether to buy something or leave the store
-     *
-     */
-    public void enterStore() {
-        Floor currentFloor;
-        char storeMovement;
-        currentLocation = findHallway(currentFloor = findFloor(currentLocation));
-        findStore(currentFloor);
-        printCurrentPosition();
-        do {
-            IO.outPutListInBox(new String[]{"[b] buy item", "[l] leave store"}, 2);
-            storeMovement = scan.next().charAt(0);
-            switch (storeMovement) {
-                case 'b' -> items();
-
-                case 'l' -> currentLocation = findHallway(currentFloor);
-                default -> System.out.println("wrong input");
-            }
-        } while (storeMovement != 'l');
-
-    }
 
     /**
      * This method is to find the hallway. Everytime the user wants to leave the store or change the
      * floor, the user must be in the hallway.
+     *
      * @param floor
      * @return
      */
@@ -176,13 +45,14 @@ public class Manager {
 
     /**
      * This method is used to find the current floor.
+     *
      * @param store
      * @return
      */
     public Floor findFloor(Store store) {
         for (Floor floor : s.getFloors()) {
             for (Store localStore : floor.getStores()) {
-                if (localStore == store) {
+                if (localStore.equals(store)) {
                     return floor;
                 }
             }
@@ -193,14 +63,16 @@ public class Manager {
     /**
      * This method is used to find the current store. The Store will
      * be saved in currentLocation.
+     *
      * @param floor
      */
     public void findStore(Floor floor) {
-        IO o = new IO();
+        Scanner scan = new Scanner(System.in);
+
         if (s.getFloors().indexOf(findFloor(currentLocation)) == 0) {
-            o.storesFirstFloor();
+            io.storesFirstFloor();
         } else {
-            o.storesSecondFloor();
+            io.storesSecondFloor();
         }
         System.out.println("store: ");
         int chooseStore = scan.nextInt();
@@ -211,9 +83,9 @@ public class Manager {
 
     /**
      * This method is used to change floors.
-     *
      */
     public void changeFloor() {
+
         Floor currentFloor = findFloor(currentLocation);
         for (Floor floor : s.getFloors()) {
             if (currentFloor != floor) {
@@ -222,46 +94,13 @@ public class Manager {
             }
         }
         currentLocation = findHallway(currentFloor);
-        printCurrentPosition();
+        io.printCurrentPosition();
     }
 
-    /**
-     * This method is used to print the current location.
-     *
-     */
-    public void printCurrentPosition() {
-        System.out.println(ANSI_BLUE + "current location: " + ANSI_RESET + (1 + s.getFloors().indexOf(findFloor(currentLocation))) + " floor" + " " + currentLocation.getName());
-    }
-
-
-    /**
-     * This method is to choose, which item the user wants to buy.
-     *
-     */
-    public void items() {
-        currentLocation.getProducts().forEach(product -> {
-            System.out.println();
-            IO.outPutListInBox(new String[]{product.getName(), product.getSize(), product.getColor(), "" + product.getPrice()}, 2);
-        });
-        System.out.println("what do you want to buy: ");
-        scan.nextLine();
-        String input = scan.nextLine();
-
-            currentLocation.getProducts().forEach(product -> {
-                if (input.equals(product.getName())) {
-                    budget -= product.getPrice();
-                }
-            });
-
-        budget = Math.round(budget / 0.05) * 0.05;
-        System.out.printf("new budget: %.2f\n", budget);
-
-    }
 
     /**
      * If the user decides to work he will land in this method. It has a randomizer with a min = 300 and max 1000. That
      * means the user can earn money between 300.- and 1000.-.
-     *
      */
     public void goWork() {
         Random r = new Random();
@@ -272,5 +111,20 @@ public class Manager {
         IO.outPutListInBox(new String[]{"your earnings: " + result}, 2);
         budget += result;
         IO.outPutListInBox(new String[]{"total budget: " + budget}, 2);
+    }
+
+    public static int readInt() {
+        Scanner scan = new Scanner(System.in);
+        int input = 0;
+        boolean isValid = false;
+        while (!isValid) {
+            try {
+                input = Integer.parseInt(scan.nextLine());
+                isValid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("wrong input");
+            }
+        }
+        return input;
     }
 }
